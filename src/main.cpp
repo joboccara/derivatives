ba722c1b74f0eb194e790219dd37cc0305e6f35f
joboc <joboc100@hotmail.com>
2014-05-04 20:47:26 +0200

    Add performance tests

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,6 @@
 
 int main()
 {
-    for (int i = 0; i < 3000; ++i)
-    {
-        compute_dfds_classic();
-    }
-    std::cout << "bu" << std::endl;
     //std::cout << "dF/dS classic = " << compute_dfds_classic() << std::endl;
     std::cout << "dF/dS algodiff = " << compute_dfds_algodiff() << std::endl;
     return 0;
